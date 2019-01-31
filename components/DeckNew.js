55bd1d2f714f12a33b4a9eac642b6be4ddd83875
George Lucas T. Bentes <george.bentes@gmail.com>
2019-01-31 04:45:46 -0400

    Handling new deck input

--- components/DeckNew.js
+++ components/DeckNew.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, TextInput } from 'react-native'
+import { Button, View, StyleSheet, Text, TextInput } from 'react-native'
 
 
 class DeckNew extends Component {
@@ -8,16 +8,51 @@ class DeckNew extends Component {
     tabBarLabel: 'New Deck'
   }
 
+  state = {
+    title: ''
+  }
+
+  onSubmit() {
+  
+    const { title } = this.state
+    const titleKey = title.replace(" ", "")
+
+  }
+
+  
+
   render() {
     return (
       <View style={{flex: 1, alignItems:'center', justifyContent:'center'}}>
         <Text>What is the title of your new Deck?</Text>
         <TextInput 
-          style={{height: 40, borderColor: 'gray', borderBottomWidth: 1, width:'90%'}}
-        />
+          style={styles.textInput}
+          onChangeText={(title) => this.setState({title})}
+          value={this.state.title} />
+
+        <Button
+          onPress={() => this.onSubmit()}
+          title="Submit"
+          style={styles.submitButton}          
+          accessibilityLabel="Save Deck"/>
+          
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  textInput: {
+    height: 40, 
+    borderColor: 'gray',
+    borderBottomWidth: 1, 
+    width:'90%',
+  },
+
+  submitButton:{
+    color:"#841584"
+  }
+})
+
 
 export default DeckNew
