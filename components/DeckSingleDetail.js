7f5c6f25999ff4189bb8bd667522a114ae8a5ad0
George Lucas T. Bentes <george.bentes@gmail.com>
2019-02-02 01:09:33 -0400

    Added file for single detail deck

---
+++ components/DeckSingleDetail.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { View, Text } from 'react-native'
+import { Button } from 'react-native-elements'
+
+class DeckSingleDetail extends React.Component { 
+  render(){
+    return (
+      <View>
+        
+      </View>
+    )
+  }
+}
+
+export default DeckSingleDetail
