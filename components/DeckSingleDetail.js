88a92222d59d298aa53108d2d2bcc852bc88e0eb
George Lucas T. Bentes <george.bentes@gmail.com>
2019-02-02 11:29:31 -0400

    Details Controls

--- components/DeckSingleDetail.js
+++ components/DeckSingleDetail.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import { View, Text } from 'react-native'
-import { Button } from 'react-native-elements'
+import { View, StyleSheet } from 'react-native'
+import { Text, Button } from 'react-native-elements'
 import { HeaderBackButton } from 'react-navigation'
 
 class DeckSingleDetail extends React.Component { 
@@ -20,11 +20,41 @@ class DeckSingleDetail extends React.Component {
 
   render(){
     return (
-      <View>
-        <Text>HAHAHAHAHAHA THE DETAIL </Text>
+      <View style={styles.container}>
+        <Text h4 style={styles.textCenter}>
+          {this.props.navigation.getParam('title')}
+        </Text>
+
+        <View style={{margin: 10}}>
+          <Text style={styles.textCenter} >0 Cards</Text>
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <Button 
+            title="Add Cards"/>
+
+          <Button
+            title="Start Quiz"/>
+        </View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  textCenter: {
+    textAlign:'center'
+  },
+
+  buttonContainer:{
+    flex:1,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+})
 
 export default DeckSingleDetail
